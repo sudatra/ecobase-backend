4d5869bd3c2be4cf4408388a20481a68860c5bd0
Vikash Kumar <vikashkumar@jmangroup.com>
2024-11-01 17:27:00 +0530

    feat: server configured

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { env } from "@/common/utils/env.config";
+import { app, logger } from "@/server";
+import { connectToDatabase, disconnectFromDatabase } from "./common/utils/db";
+
+const bootstrap = async () => {
+    await connectToDatabase();
+    const server = app.listen(env.PORT, () => {
+        const { NODE_ENV, HOST, PORT } = env;
+        logger.info(`Server ${NODE_ENV} running on port http://${HOST}:${PORT}`);
+    })
+
+    const onCloseSignal = async () => {
+        logger.info("sigint received, shutting down");
+
+        server.close(async () => {
+            logger.info("server closed");
+            await disconnectFromDatabase();
+            logger.info("prisma client disconnected");
+            process.exit();
+        });
+
+        setTimeout(() => process.exit(1), 10000).unref();
+    };
+
+    process.on("SIGINT", onCloseSignal);
+    process.on("SIGTERM", onCloseSignal);
+}
+
+bootstrap().catch((error) => {
+    logger.error("Failed to start the server", error);
+    process.exit();
+});
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,35 @@
 import cors from 'cors';
-import express, {type Express} from "express";
+import express, { type Express } from "express";
 import helmet from "helmet";
-import {pino} from "pino";
+import { pino } from "pino";
 
-import {authRouter} from "@/api/auth/auth.route";
+import { authRouter } from "@/api/auth/auth.route";
 import errorHandler from '@/common/middlewares/error.handler';
 import requestLogger from '@/common/middlewares/request.logger';
-import {env} from "@/common/utils/env.config";
+import { env } from "@/common/utils/env.config";
+import rateLimiter from '@/common/middlewares/rate.limiter';
 
-const logger = pino({name:"server start"});
+const logger = pino({ name: "server start" });
+const app: Express = express();
+
+// Set the application to trust the reverse proxy
+app.set("trust proxy", true);
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use(helmet());
+app.use(rateLimiter)
+
+// Request logging
+app.use(requestLogger);
+
+// Routes
+app.use("/auth",authRouter);
+
+app.use(errorHandler());
+
+export {app, logger};
+
+
